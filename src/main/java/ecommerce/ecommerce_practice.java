8b8ce25abdfec2ff3c0112f9f973fe67064383ec
Swayam Mohanty <swayam0506@gmail.com>
2022-01-18 13:58:58 +0530

    modified ecommerce addToCart comments

--- src/main/java/ecommerce/ecommerce_practice.java
+++ src/main/java/ecommerce/ecommerce_practice.java
@@ -22,26 +22,25 @@ public class ecommerce_practice {
 		// url
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
 
-		String[] productsstock = {"Brocolli", "Cauliflower", "Cucumber", "Beetroot"};
+		String[] productsstock = { "Brocolli", "Cauliflower", "Cucumber", "Beetroot" };
 
 		List<WebElement> allproductsname = driver.findElements(By.xpath("//h4[@class='product-name']"));
 
 		int count = 0;
 		for (int i = 0; i < allproductsname.size(); i++) {
-			//String individualproductname = allproductsname.get(i).getText();
-			
-			//Backened datas=Ex:Cauliflower - 1 Kg
-			//we dont need -1kg so we are doing string spliting 
+			// String individualproductname = allproductsname.get(i).getText();
+
+			// Backened datas=Ex:Cauliflower - 1 Kg
+			// we dont need -1kg so we are doing string spliting
 			String[] splitedname = allproductsname.get(i).getText().split("-");
 			String trimmedproductname = splitedname[0].trim();
-			//System.out.println(trimmedproductname);
 
-			//convert array to arrylists because arraylists has contains method
+			// convert array to arrylists because arraylists has contains method
 			List al = Arrays.asList(productsstock);
-            //here we comparing our stock product with trimmed product
+			// here we comparing our stock product with trimmed product
 			if (al.contains(trimmedproductname)) {
-				System.out.println("product name:"+trimmedproductname+" "+count+" "+"clicked...");
-				//Here clicking happens based on products position
+				System.out.println("product name:" + trimmedproductname + " " + count + " " + "clicked...");
+				// Here clicking happens based on products position
 				driver.findElements(By.xpath("//div[@class=\"product-action\"]/button")).get(i).click();
 			}
 			count++;
